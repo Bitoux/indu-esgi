bd0c284226f99d6093f5d6453785587717f4f972
Unknown <yann.miloux@gmail.com>
2018-02-16 10:45:09 +0100

    Travis \\ Fix function

--- web/index.php
+++ web/index.php
@@ -6,7 +6,9 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 // Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array('monolog.logfile' => 'php://stderr'));
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr'
+));
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
